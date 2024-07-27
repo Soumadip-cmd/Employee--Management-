d8cfc5d9b31ae44181703556c31e9f4ed3d29bd3
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-27 17:34:07 +0530

    updated profile dropdown

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,47 +1,70 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        closeDropdown();
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <>
       <nav
-        className=" d-flex justify-content-between ps-4 pe-2 align-items-center "
-        style={{ background: "#112a47", height: "50px",position:'fixed',zIndex:'20',width:'100%' }}
+        className="d-flex justify-content-between ps-4 pe-2 align-items-center"
+        style={{ background: "#112a47", height: "50px", position: 'fixed', zIndex: '20', width: '100%' }}
       >
         <NavLink
-        to='/'
+          to='/'
           className="float-start fw-bold text-decoration-none"
           style={{ fontFamily: '"Playwrite US Modern", cursive' }}
         >
-          <span className=" opacity-80" style={{color:'#f55757'}}>Employee</span>{" "}
+          <span className="opacity-80" style={{ color: '#f55757' }}>Employee</span>{" "}
           <span className="text-info">Management</span>
         </NavLink>
-        {/* <span className="float-end text-white mx-2 px-2 py-1" style={{border:'1px solid white', borderRadius:'50%',cursor:'pointer'}}><i className=" fa-regular fa-user fa"></i></span> */}
 
-        <div className="flex-shrink-0 dropdown mx-2 mx-lg-3">
+        <div className="flex-shrink-0 dropdown mx-2 mx-lg-3" ref={dropdownRef}>
           <NavLink
             to="/"
             className="d-block link-body-emphasis text-decoration-none dropdown-toggle dropdown-toggle-no-caret"
             data-bs-toggle="dropdown"
             aria-expanded="false"
+            onClick={toggleDropdown}
           >
             <img
               src="https://placehold.co/32x32"
-              alt="mdo"
+              alt="profile"
               width="32"
               height="32"
               className="rounded-circle"
             />
           </NavLink>
-          <ul className="dropdown-menu text-small shadow">
-           
+          <ul className={`dropdown-menu text-small shadow ${dropdownOpen ? 'show' : ''}`}>
             <li>
-              <a className="dropdown-item" href="#">
-                Settings
-              </a>
+              <NavLink className="dropdown-item" to="/" onClick={closeDropdown}>
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink className="dropdown-item" to="/profile">
+              <NavLink className="dropdown-item" to="/profile" onClick={closeDropdown}>
                 Profile
               </NavLink>
             </li>
@@ -49,7 +72,7 @@ const NavBar = () => {
               <hr className="dropdown-divider" />
             </li>
             <li>
-              <NavLink className="dropdown-item" to="/login">
+              <NavLink className="dropdown-item" to="/login" onClick={closeDropdown}>
                 Sign out
               </NavLink>
             </li>
