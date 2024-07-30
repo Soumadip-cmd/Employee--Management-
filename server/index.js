679d4f0e3840defcac0e5ad086ee23a8f418bbd8
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-07-30 21:16:21 +0530

    admin delete

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json());
-app.use(require(path.join(__dirname, "Routes/Admin.js")));
 app.use(require(path.join(__dirname, "Routes/Department.js")));
 app.use(require(path.join(__dirname, "Routes/Leave.js")));
 app.use(require(path.join(__dirname, "Routes/Salary.js")));
