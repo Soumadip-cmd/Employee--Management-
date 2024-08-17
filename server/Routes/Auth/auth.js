6ba859aa304fb49814bb0a45b6f1f9219b66fe70
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-08-17 15:00:50 +0530

    changing updatwed tales time

--- server/Routes/Auth/auth.js
+++ server/Routes/Auth/auth.js
@@ -356,7 +356,8 @@ router.post("/forget-password", async (req, res) => {
       { expiresIn: "1h" }
     );
 
-    const link = `http://localhost:8800/reset-password/${oldPassword._id}/${token}`;
+    // const link = `http://localhost:8800/reset-password/${oldPassword._id}/${token}`;
+    const link = `https://employee-management-uyw0.onrender.com/reset-password/${oldPassword._id}/${token}`;
     var transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
