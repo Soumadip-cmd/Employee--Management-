8309ac2c4ad2a0669c615ba7ad570ae859a7ecaa
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-12-05 19:51:37 +0530

    check done

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -129,8 +129,7 @@ const Profile = () => {
   if (isLoading) {
     return (
       <div 
-        className="vh-100 vw-100 position-fixed top-0 start-0 d-flex justify-content-center align-items-center bg-white"
-        style={{ zIndex: 1000 }}
+        
       >
         <Loading />
       </div>
